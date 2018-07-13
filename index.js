20eabf65d29c029f248e9aa2db19d86536361f0b
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-07-13 19:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var recipes= {};
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
-function destri
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
